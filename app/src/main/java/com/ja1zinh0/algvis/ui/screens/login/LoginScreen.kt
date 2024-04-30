8205594c732f0b68f2deb2c812872e04425c8e78
Ja1zinh0 <joao2011bha@hotmail.com>
2024-04-30 16:45:39 -0300

    add loginScreen layout

--- app/src/main/java/com/ja1zinh0/algvis/ui/screens/login/LoginScreen.kt
+++ app/src/main/java/com/ja1zinh0/algvis/ui/screens/login/LoginScreen.kt
@@ -2,13 +2,18 @@ package com.ja1zinh0.algvis.ui.screens.login
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -28,8 +33,10 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.compose.md_theme_dark_onSecondary
 import com.example.compose.md_theme_dark_secondaryContainer
 import com.ja1zinh0.algvis.R
 
@@ -54,8 +61,34 @@ fun LoginScreen() {
                 style = TextStyle(color = Color.White, fontSize = 40.sp),
                 fontStyle = FontStyle.Italic,
                 fontWeight = FontWeight.Light,
+                modifier = Modifier.padding(bottom = 10.dp, top = 30.dp)
             )
             CustomTextField(label = "Email", onValueChange = {})
+            Box(modifier = Modifier.height(20.dp)){}
+            CustomTextField(label = "Senha", onValueChange = {})
+            Box(modifier = Modifier.padding(bottom = 55.dp))
+            OutlinedButton(
+                onClick = {},
+                modifier = Modifier
+                    .size(220.dp, 50.dp)
+                    .border(
+                        width = 1.dp,
+                        color = Color.White,
+                        shape = RoundedCornerShape(50.dp)
+                    ),
+                colors = ButtonDefaults.buttonColors(containerColor = md_theme_dark_onSecondary)
+            ) {
+                Text(
+                    text = "Login",
+                    style = TextStyle(
+                        fontSize = 24.sp,
+                        textAlign = TextAlign.Center,
+                        color = Color.White
+                    ),
+                    fontStyle = FontStyle.Italic,
+                    fontWeight = FontWeight.Light,
+                )
+            }
         }
     }
 }
